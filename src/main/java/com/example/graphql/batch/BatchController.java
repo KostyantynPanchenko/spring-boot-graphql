8d05789635d74534f766b00134692106866263b2
Kostyantyn Panchenko <yougetit@ukr.net>
2023-03-07 15:38:34 +0200

    Add batch mapping to get rid of n+1 problem for fetching post comments

--- src/main/java/com/example/graphql/batch/BatchController.java
+++ src/main/java/com/example/graphql/batch/BatchController.java
@@ -5,6 +5,7 @@ import com.example.graphql.model.PostComment;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 class BatchController {
 
-  private final Map<Post, Collection<PostComment>> comments = Map.of(
+  private final Map<Post, Collection<PostComment>> postComments = Map.of(
       new Post(1, "Russia attacks Ukraine", "Russia is a terrorist state"),
       List.of(new PostComment(1, "+1"), new PostComment(1, "+11")),
       new Post(2, "Putin must d i e", "Puck Futin"),
@@ -20,12 +21,19 @@ class BatchController {
 
   @QueryMapping
   Collection<Post> posts() {
-    return comments.keySet();
+    return postComments.keySet();
   }
 
+  @BatchMapping(typeName = "Post", field = "comments")
+  Map<Post, Collection<PostComment>> postComments(Collection<Post> posts) {
+    return postComments;
+  }
+
+  /*
   @SchemaMapping(typeName = "Post", field = "comments")
   Collection<PostComment> postComments(Post post) {
     System.out.println("Fetching post comment for post ID = " + post.id());
-    return comments.get(post);
+    return postComments.get(post);
   }
+  */
 }
